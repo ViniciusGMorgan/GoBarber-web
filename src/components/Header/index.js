909de887528acb462cb9e6c941ad1d5c8f4f6bce
ViniciusGMorgan <45949731+ViniciusGMorgan@users.noreply.github.com>
2020-07-24 08:46:10 -0300

    Add files via upload

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/logo-purple.svg";
+
+import Notifications from "../Notifications";
+
+import { Container, Content, Profile } from "./styles";
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber"></img>
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Notifications></Notifications>
+          <Profile>
+            <div>
+              <strong>Vinícius Morgan</strong>
+              <Link to="/profile">Meu perfil</Link>
+              <img src="" alt=""></img>
+            </div>
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
---
+++ src/pages/_layouts/default/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Wrapper } from "./styles";
+
+import Header from "../../../components/Header";
+
+export default function DefaultLayout({ children }) {
+  return (
+    <Wrapper>
+      <Header>{children}</Header>
+    </Wrapper>
+  );
+}
